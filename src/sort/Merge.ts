4d3b93a0bb04602833ed52b89954ea92f3078075
Jesse Cascio <jesse.cascio@timeinc.com>
2017-03-02 20:10:21 -0800

    merge sort

--- src/sort/Merge.ts
+++ src/sort/Merge.ts
@@ -4,88 +4,52 @@
  */
 export default class Merge<V> {
 
-  // https://www.khanacademy.org/computing/computer-science/algorithms/merge-sort/a/overview-of-merge-sort
   public static sort(data: any[]): any[] {
     if (!data.length) {
       return data;
     }
 
-    let chunks = [];
+    Merge._sort(data, 0, data.length);
 
-    while (data.length) {
-      chunks.push([data.pop()]);
-    }
-
-    if (chunks.length <= 1) {
-      return chunks.pop();
-    }
-
-    chunks = this._syncDown(chunks);
-
-    while (chunks.length !== 1) {
-      chunks = this._syncDown(chunks);
-    }
-
-    return chunks.pop();
+    return data.filter(val => val);
   }
 
-  // syncs an array down by combing adjoining arrays
-  private static _syncDown(array) {
-    const combined = [];
+  private static _sort(data, from, to) {
 
-    for (let i = 0; i < array.length + 1; i += 2) {
-      if (!array[i]) {
-        continue;
-      }
-      if (!array[(i + 1)]) {
-        combined.push(array[i]);
-        continue;
-      }
-
-      combined.push(Merge._merge(array[i], array[(i + 1)]));
+    if (to <= from) {
+      return;
     }
 
-    return combined;
+    const mid = Math.floor((from + to) / 2);
+
+    Merge._sort(data, from, mid);
+    Merge._sort(data, mid + 1, to);
+
+    Merge._merge(data, from, mid, to);
   }
 
-  // merge two array, maintaining order
-  private static _merge(arr1, arr2): any[] {
-    if (!arr1 && !arr2) {
-      return [];
-    }
-    if (!arr1) {
-      return arr2;
-    }
-    if (!arr2) {
-      return arr1;
+  private static _merge(data, from, mid, to): void {
+
+    const copy = [];
+
+    let x = from;
+    let y = mid + 1;
+
+    for (let z = from; z <= to; z++) {
+      copy[z] = data[z];
     }
 
-    let x = 0;
-    let y = 0;
-
-    const sorted = [];
-
-    while (sorted.length < arr1.length + arr2.length) {
-      if (!arr1[x]) {
-        sorted.push(arr2[y]);
-        y++;
-        continue;
-      }
-      if (!arr2[y]) {
-        sorted.push(arr1[x]);
-        x++;
-        continue;
-      }
-      if (arr1[x] < arr2[y]) {
-        sorted.push(arr1[x]);
-        x++;
-      } else {
-        sorted.push(arr2[y]);
-        y++;
+    for (let z = from; z <= to; z++) {
+      if (x > mid ) {
+        data[z] = copy[y++];
+      } else if (y > to) {
+        data[z] = copy[x++];
+      } else if (copy[x] > copy[y]) {
+        data[z] = copy[y++];
+      }  else {
+        data[z] = copy[x++];
       }
     }
-
-    return sorted;
   }
 
 }
