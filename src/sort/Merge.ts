e05f3a1b65bda028f5678a0711ecb63624e38008
Jesse Cascio <jesse.cascio@timeinc.com>
2017-03-31 16:22:16 -0700

    alt merge sort

--- src/sort/Merge.ts
+++ src/sort/Merge.ts
@@ -9,11 +9,64 @@ export default class Merge<V> {
       return data;
     }
 
-    Merge._sort(data, 0, data.length);
-
-    return data.filter(val => val);
+    return Merge._sort(data);
   }
 
+  private static _sort(data: any[]): any[] {
+    if (!data.length || data.length === 1) {
+      return data;
+    }
+
+    const mid = Math.floor(data.length / 2);
+
+    const sub1 = Merge._sort(data.slice(0, mid));
+    const sub2 = Merge._sort(data.slice(mid));
+
+    return Merge._merge(sub1, sub2);
+  }
+
+  private static _merge(sub1, sub2) {
+    if (!sub1.length && !sub2.length) {
+      return [];
+    } else if (!sub1.length) {
+      return sub2;
+    } else if (!sub2.length) {
+      return sub1;
+    }
+
+    const merged = [];
+    let i = 0;
+    let j = 0;
+
+    while (i < sub1.length || j < sub2.length) {
+      if (i === sub1.length) {
+        merged.push(sub2[j]);
+        j++;
+      } else if (j === sub2.length) {
+        merged.push(sub1[i]);
+        i++;
+      } else if (sub1[i] < sub2[j]) {
+        merged.push(sub1[i]);
+        i++;
+      } else if (sub2[j] < sub1[i]) {
+        merged.push(sub2[j]);
+        j++;
+      } else {
+        merged.push(sub1[i]);
+        merged.push(sub2[j]);
+        i++;
+        j++;
+      }
+    }
+
+    return merged;
+  }
+
+  /*
+   * CONSTANT SPACE
+   */
+
+  /*
   private static _sort(data, from, to) {
 
     if (to <= from) {
@@ -51,5 +104,5 @@ export default class Merge<V> {
       }
     }
   }
-
+  */
 }
